becc3fde147ac65a279a4ccb08aacb102ac2495b
WangRunhui <576156676@qq.com>
2015-03-31 17:09:27 +0800

    Hehe

---
+++ app/src/main/java/com/example/sixinwen/NewsShow.java
@@ -0,0 +1,18 @@
+package com.example.sixinwen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kakarotto on 3/26/15.
+ */
+public class NewsShow extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.news_show);
+
+    }
+}
--- app/src/main/java/com/example/sixinwen/fragments/FirstPageHome.java
+++ app/src/main/java/com/example/sixinwen/fragments/FirstPageHome.java
@@ -1,15 +1,18 @@
 package com.example.sixinwen.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import com.example.sixinwen.NewsShow;
 import com.example.sixinwen.R;
 import com.example.sixinwen.adapter.FirstPageNewsAdapter;
 import com.example.sixinwen.utils.NewsItem;
@@ -35,6 +38,13 @@ public class FirstPageHome extends Fragment{
     public void onStart() {
         super.onStart();
         init();
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(),NewsShow.class);
+                startActivity(intent);
+            }
+        });
     }
     void init() {
         mListView = (ListView)getActivity().findViewById(R.id.first_page_listview);
@@ -43,7 +53,6 @@ public class FirstPageHome extends Fragment{
         imageView.setImageResource(R.drawable.win10tx);
         mAdapter = new FirstPageNewsAdapter(getActivity(), newsItemList);
         NewsItem newsItem = new NewsItem("微软腾讯战略合作","微软腾讯战略合作 电脑管家可一键升级Win10",4,3,imageView);
-        newsItemList.add(newsItem);
         newsItemList.add(newsItem);
         newsItemList.add(newsItem);
         newsItemList.add(newsItem);
