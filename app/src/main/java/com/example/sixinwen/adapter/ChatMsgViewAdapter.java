efbf0e2a57a7144fa6d53e1a4c22acf52187535b
WangRunhui <576156676@qq.com>
2015-04-11 03:03:09 +0800

    Add chat window

---
+++ app/src/main/java/com/example/sixinwen/adapter/ChatMsgViewAdapter.java
@@ -0,0 +1,7 @@
+package com.example.sixinwen.adapter;
+
+/**
+ * Created by kakarotto on 15-3-31.
+ */
+public class ChatMsgViewAdapter {
+}
---
+++ app/src/main/java/com/example/sixinwen/utils/ChatMsgEntity.java
@@ -0,0 +1,7 @@
+package com.example.sixinwen.utils;
+
+/**
+ * Created by kakarotto on 15-3-31.
+ */
+public class ChatMsgEntity {
+}
